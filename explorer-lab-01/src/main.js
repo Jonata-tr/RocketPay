4bdaf08a6f5d1ce4ebb3f09c568ac210f4815782
Jonatan <Jonatatrindasilva@gmail.com>
2022-10-21 20:41:18 -0400

    Finalização do projeto e configuração das mascaras

--- explorer-lab-01/src/main.js
+++ explorer-lab-01/src/main.js
@@ -7,13 +7,12 @@ const ccBgColor01 = document.querySelector(
 const ccBgColor02 = document.querySelector(
   ".cc-bg svg > g g:nth-child(2) path"
 );
-const ccLogo = document.querySelector(".cc-log span:nth-child(2) img");
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img");
 
 function setCardType(type) {
   const colors = {
     visa: ["#0ea5e9", "#2563eb"],
     mastercard: ["#f59e0b", "#dc2626"],
-    c6: ["#78716c", "#fafafa"],
     default: ["black", "gray"],
   };
 
@@ -22,7 +21,7 @@ function setCardType(type) {
   ccLogo.setAttribute("src", `cc-${type}.svg`);
 }
 
-globalThis.setCardType = setCardType;
+globalThis.setCardType = setCardType
 
 const securityCode =document.querySelector("#security-code")
 const securityCodePattern = {
@@ -58,12 +57,8 @@ const cardNumberPattern = {
         },
         {
             mask: "0000 0000 0000 0000",
-            regex: /(^5[1-5]\{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+            regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
             cardtype: "mastercard",
-        },
-        {
-            mask: "0000 0000 0000 0000",
-            cardtype: "c6",
         },
         {
             mask: "0000 0000 0000 0000",
@@ -76,9 +71,57 @@ const cardNumberPattern = {
             return number.match(item.regex)
         })    
 
-        console.log(foundMask)
         return foundMask
     }, 
 }
 
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+const addButton = document.querySelector("#add-card")
+addButton.addEventListener("click", () => {
+    
+} )
+
+document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault()
+})
+
+const cardHolder = document.querySelector("#card-holder")
+cardHolder.addEventListener("input", () => {
+    const ccHolder = document.querySelector(".cc-holder .value")
+    ccHolder.innerText  = cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value 
+}
+)
+
+securityCodeMasked.on("accept", () =>{
+    updateSecurityCode(securityCodeMasked.value)
+})
+
+function updateSecurityCode(code){
+    const ccSecurity = document.querySelector(".cc-security .value")
+    ccSecurity.innerText = code.length === 0 ? "123" : code 
+}
+
+cardNumberMasked.on("accept", () => {
+    updateCardNumber(cardNumberMasked.value)
+})
+
+cardNumberMasked.on("accept", () => {
+    const cardType = cardNumberMasked.masked.currentMask.cardtype
+    setCardType(cardType)
+    updateCardNumber(cardNumberMasked.value)
+})
+
+function updateCardNumber(number){
+    const ccNumber = document.querySelector(".cc-number")
+    ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456": number
+}
+
+expirationDateMasked.on("accept", () => {
+    updateExpirationDate(expirationDateMasked.value)
+})
+
+function updateExpirationDate(date){
+    const ccExpiration = document.querySelector(".cc-extra .value")
+    ccExpiration.innerText = date.length === 0 ? "02/32" : date
+} 
